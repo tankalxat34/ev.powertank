d7a6c2b94ef2b60a9bb716424628b91a94099c3f
Alexander Podstrechnyy <tankalxat34@gmail.com>
2023-11-16 23:57:28 +0300

    Сохранены необходимые шрифты

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { observer } from 'mobx-react-lite';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>EV PowerTank</h1>
+      <p>Первый сервис мобильных зарядных станций в Москве</p>
     </div>
   );
 }
