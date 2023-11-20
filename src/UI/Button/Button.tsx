5f5d9eef2064b12af887ac43729275071b14a907
Alexander Podstrechnyy <tankalxat34@gmail.com>
2023-11-20 20:47:31 +0300

    убрал заглушку у кнопки

--- src/UI/Button/Button.tsx
+++ src/UI/Button/Button.tsx
@@ -5,16 +5,11 @@ import Modal from "../Modal/Modal";
 
 export const Button: React.FC<React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
 > = (props) => {
-    const [_modalErrorAction, setModalErrorAction] = useState(false);
     return (
         <>
-            <button className={`btn ${props.type || 'primary'}`} {...props} onClick={props.onClick ? props.onClick : () => setModalErrorAction(true)}>
+            <button className={`btn ${props.type || 'primary'}`} {...props}>
                 {props.children}
             </button>
-            <Modal active={_modalErrorAction} setActive={setModalErrorAction} style={{textAlign: "left!important"}}>
-                <h3>Ошибка</h3>
-                Это действие пока недоступно!
-            </Modal>
         </>
     )
 }
