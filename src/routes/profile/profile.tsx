1b81592120078d63ebf1223f84b4778152a03ae4
Alexander Podstrechnyy <tankalxat34@gmail.com>
2023-11-23 13:18:37 +0300

    Типизировал данные о пользователе & начал верстку

--- src/routes/profile/profile.tsx
+++ src/routes/profile/profile.tsx
@@ -3,20 +3,24 @@ import { SectionText } from "../../UI/Section/Section";
 import { PageLoader } from "../../components/LkState/LkState";
 import { userFirebaseContext } from "../../App";
 import { isEmpty } from "lodash";
+import styles from './profile.module.css';
+import { User } from "firebase/auth";
+import { IDBUser } from "../../interfaces/IFirestoreDb";
 
 
 const Profile: React.FC = () => {
 
-    const {userData, dbData} = useContext(userFirebaseContext) as any;
+    const {userData, dbData} = useContext(userFirebaseContext) as {userData: User, dbData: IDBUser};
 
     console.log(userData, dbData);
 
     if (isEmpty(userData) && isEmpty(dbData)) {
-        return <SectionText style={{ minHeight: '90vh', marginTop: '10vh' }}>
+        return <SectionText className={styles.profileSection}>
             <PageLoader />
         </SectionText>
     }
-    return <SectionText style={{minHeight: '90vh'}}>
+
+    return <SectionText className={styles.profileSection}>
         <h2>{userData.displayName}</h2>
     </SectionText>
 }
